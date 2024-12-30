2285d3f482bc887c21ba11df925a6b2bcf8daf2f
sarmaakondi <avnmsarma@gmail.com>
2024-12-30 17:54:58 +1100

    Create handleClick

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,4 +1,12 @@
 /* eslint-disable react/prop-types */
 export default function Square({ value }) {
-    return <button className="square">{value}</button>;
+    function handleClick() {
+        console.log("clicked");
+    }
+
+    return (
+        <button className="square" onClick={handleClick}>
+            {value}
+        </button>
+    );
 }
