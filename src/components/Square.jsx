75a8a1e418c2ec56ff8a244cf79bae661843f7b9
sarmaakondi <avnmsarma@gmail.com>
2024-12-30 18:25:20 +1100

    Lifting state up

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
-
-export default function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        setValue("X");
-    }
-
+/* eslint-disable react/prop-types */
+export default function Square({ value, onSquareClick }) {
     return (
-        <button className="square" onClick={handleClick}>
+        <button className="square" onClick={onSquareClick}>
             {value}
         </button>
     );
